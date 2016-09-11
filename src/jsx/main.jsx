0d0c6e5c59ac36ea9a464f89e7a2ca67316097ac
Katie Siegel <katiesiegel@samsara.com>
2016-09-10 19:53:54 -0700

    Modal shows and hides by clicking appropriate buttons

--- src/jsx/main.jsx
+++ src/jsx/main.jsx
@@ -2,18 +2,28 @@ var React = require("react");
 var ReactDOM = require("react-dom");
 var SurveyModal = require("./survey_modal.jsx");
 var DataDisplay = require("./data_display.jsx");
+var Button = require("react-bootstrap").Button;
 
 var Container = React.createClass({
   getInitialState: function() {
     return {
-      showSurvey: false
+      showSurvey: false,
+      isAuthenticated: false
     };
+  },
+
+  showSurveyModal: function() {
+    this.setState({showSurvey: true});
+  },
+
+  hideSurveyModal: function() {
+    this.setState({showSurvey: false});
   },
 
   render: function() {
     let modal;
     if (this.state.showSurvey) {
-      modal = (<SurveyModal />);
+      modal = (<SurveyModal hideSurvey={this.hideSurveyModal} isAuthenticated={this.state.isAuthenticated}/>);
     }
     return(
       <div>
@@ -21,7 +31,11 @@ var Container = React.createClass({
           <div className="container-fluid">
             <div className="navbar-header">
               <a className="navbar-brand" href="#">New-Grad Salaries</a>
-              <button className="btn" id="take-survey-btn">Submit Data</button>
+            </div>
+            <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+              <ul className="nav navbar-nav navbar-right">
+                <li><Button onClick={this.showSurveyModal} id="take-survey-btn">Submit Data</Button></li>
+              </ul>
             </div>
           </div>
         </div>
--- src/jsx/survey_modal.jsx
+++ src/jsx/survey_modal.jsx
@@ -1,8 +1,37 @@
 var React = require("react");
+var Modal = require("react-bootstrap").Modal;
+var Button = require("react-bootstrap").Button;
 
 var SurveyModal = React.createClass({
+  propTypes: {
+    hideSurvey: React.PropTypes.func.isRequired,
+    isAuthenticated: React.PropTypes.bool.isRequired
+  },
+
   render: function() {
-    return (<div>TODO modal</div>);
+    let submitButton;
+    if (this.props.isAuthenticated) {
+      submitButton = (<Button bsStyle="primary">Save changes</Button>);
+    }
+    const modalInstance = (
+      <div className="static-modal">
+        <Modal.Dialog>
+          <Modal.Header>
+            <Modal.Title>Take the survey!</Modal.Title>
+          </Modal.Header>
+
+          <Modal.Body>
+            Contribute to this project by adding your own new-grad salary data.
+          </Modal.Body>
+
+          <Modal.Footer>
+            <Button onClick={this.props.hideSurvey}>Close</Button>
+          </Modal.Footer>
+
+        </Modal.Dialog>
+      </div>
+    );
+    return modalInstance;
   }
 });
 
