09f949d05f0c074eb4ead08dff56750d602090fb
Katie Siegel <katiesiegel@samsara.com>
2016-09-10 21:06:51 -0700

    app.js and route cleanup

--- app.js
+++ app.js
@@ -11,7 +11,6 @@ var config = require('./config.json');
 var User = require('./models/user.js');
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
 
 var app = express();
 
@@ -62,7 +61,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var passport = require('passport');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Test' });
+  res.render('index', { title: 'New-Grad Salaries' });
 });
 
 router.get('/auth/facebook',
